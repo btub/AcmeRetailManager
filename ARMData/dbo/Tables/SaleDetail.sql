4f0711bf67a6ba1ac9af3a11a9ff56892fe879e5
buse t kocabıyık <btubakocabiyik@gmail.com>
2022-04-05 21:16:29 +0300

    Add Foreign Key Relationships
    Adds FK relationships on tables.

--- ARMData/dbo/Tables/SaleDetail.sql
+++ ARMData/dbo/Tables/SaleDetail.sql
@@ -2,9 +2,11 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [SaleId] INT NOT NULL, 
-    [ProdeuctId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1,
     [PurchasePrice] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL DEFAULT 0
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleId) REFERENCES Sale(Id), 
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id)
     
 )
