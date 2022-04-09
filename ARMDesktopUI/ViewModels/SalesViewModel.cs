bcaeb4c92866c793ef619a4143c43b3af6f033d2
buse t kocabıyık <btubakocabiyik@gmail.com>
2022-04-09 22:02:01 +0300

    Add Remove From Cart Functionality

--- ARMDesktopUI/ViewModels/SalesViewModel.cs
+++ ARMDesktopUI/ViewModels/SalesViewModel.cs
@@ -68,6 +68,20 @@ namespace ARMDesktopUI.ViewModels
             }
         }
 
+
+        private CartItemDisplayModel _selectedCartItem;
+
+        public CartItemDisplayModel SelectedCartItem
+        {
+            get { return _selectedCartItem; }
+            set
+            {
+                _selectedCartItem = value;
+                NotifyOfPropertyChange(() => SelectedCartItem);
+                NotifyOfPropertyChange(() => CanRemoveFromCart);
+            }
+        }
+
         private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
 
         public BindingList<CartItemDisplayModel> Cart
@@ -184,12 +198,24 @@ namespace ARMDesktopUI.ViewModels
             {
                 bool output = false;
                 // Make sure something is selected
+                if(SelectedCartItem != null && SelectedCartItem?.QuantityInCart > 0)
+                    output = true;
                 return output;
             }
         }
 
         public void RemoveFromCart()
         {
+            SelectedCartItem.Product.QuantityInStock += 1;
+            
+            if (SelectedCartItem.QuantityInCart>1)
+            {
+                SelectedCartItem.QuantityInCart -= 1;
+            }
+            else
+            {
+                Cart.Remove(SelectedCartItem);
+            }
             NotifyOfPropertyChange(() => SubTotal);
             NotifyOfPropertyChange(() => Tax);
             NotifyOfPropertyChange(() => Total);
